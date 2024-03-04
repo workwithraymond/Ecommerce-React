ee112585eb234f0472ef2b0a8a91d9e02c6879d9
workwithraymond <workwithraymond01@gmail.com>
2024-03-04 18:32:12 -0500

    new commit

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+https://github.com/john-smilga/react-course-comfy-sloth-store  
+https://react-course-comfy-sloth-store.netlify.app/
+
+https://react-projects.netlify.app/ 
+
+npm install axios
+npm install react-router-dom
+npm install react-icons
+npm install styled-components
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+import styled from 'styled-components'
 
+const Button = styled.button`
+  background: green;
+  color: white;
+`
+const Container = styled.div`
+  background: red;
+  color: white;
+  .hero {
+     font-size: 8rem;
+  }
+ 
+`
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h4>comfy sloth starter</h4>
+      <Button>click me</Button>
+      <Container>
+        <div>
+          <h3>
+            hello world
+          </h3>
+        </div>
+        <div className='hero'>
+        hero text
+        </div>
+      </Container>
+    </div>
   )
 }
 
