1f54bc275930e2a824895042e0faf60ef451839e
workwithraymond <workwithraymond01@gmail.com>
2024-03-05 19:36:59 -0500

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
-import styled from 'styled-components'
+import { Home, SingleProduct, Cart, Checkout, Error, About, Product, PrivateRoute } from './pages'
 
-const Button = styled.button`
-  background: green;
-  color: white;
-`
-const Container = styled.div`
-  background: red;
-  color: white;
-  .hero {
-     font-size: 8rem;
-  }
- 
-`
+
+
+
 function App() {
   return (
-    <div>
-      <h4>comfy sloth starter</h4>
-      <Button>click me</Button>
-      <Container>
-        <div>
-          <h3>
-            hello world
-          </h3>
-        </div>
-        <div className='hero'>
-        hero text
-        </div>
-      </Container>
-    </div>
+    <Router>
+      <Navbar />
+      <Sidebar />
+     <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route exact path='/about'>
+        <About />
+      </Route>
+      <Route exact path='/cart'>
+        <Cart />
+      </Route>
+      <Route exact path='/products'>
+        <Product />
+      </Route>
+      <Route exact path='/product/:id' children={<SingleProduct/>}/>
+    
+      <Route exact path='/checkout'>
+        <Checkout />
+      </Route>
+      <Route path='*'>
+        <Error />
+      </Route>
+     </Switch>
+     <Footer />
+    </Router>
   )
 }
 
