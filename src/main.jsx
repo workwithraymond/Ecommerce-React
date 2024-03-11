03401503b50bb29489cdae37500a5ed4e142e291
workwithraymond <workwithraymond01@gmail.com>
2024-03-10 20:19:11 -0400

    new changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { ProductsProvider } from './context/products_context';
 import { FilterProvider } from './context/filter_context';
 import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
-// import { Auth0Provider } from '@auth0/auth0-react';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ProductsProvider>
-    <App />
+    <FilterProvider>
+    <App />  
+    </FilterProvider>
+    
   </ProductsProvider>,
 )
 
